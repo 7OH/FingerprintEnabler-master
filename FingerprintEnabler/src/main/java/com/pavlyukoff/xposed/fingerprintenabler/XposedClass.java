c0319c5d660af02e6ac31a122bc2a80636ce7e9c
7OH <pavlyukoff@gmail.com>
2016-07-05 00:29:26 +0300

    Initial commit

---
+++ FingerprintEnabler/src/main/java/com/pavlyukoff/xposed/fingerprintenabler/XposedClass.java
@@ -0,0 +1,66 @@
+package com.pavlyukoff.xposed.fingerprintenabler;
+
+import android.app.Application;
+import android.util.Log;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+public class XposedClass extends Application implements IXposedHookLoadPackage {
+	private static final String TAG = "xposed_debug";
+	
+	public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+		
+		/*
+		if (lpparam.packageName.equals("com.android.internal"))
+		{
+
+			findAndHookMethod("com.android.internal.widget.LockPatternUtils$StrongAuthTracker", lpparam.classLoader,
+					"isFingerprintAllowedForUser", int.class, new XC_MethodReplacement()
+					{
+						@Override
+						protected Object replaceHookedMethod(MethodHookParam param) throws Throwable
+						{
+							Log.e("fpEnabler", "isFingerprintAllowedForUser TRUE");
+							return true;
+						}
+					}
+			);
+			Log.e(TAG, "Hooked : " + lpparam.packageName.toString());
+
+		}
+		*/
+
+		if (lpparam.packageName.equals("com.android.systemui")) {
+
+			findAndHookMethod("com.android.keyguard.KeyguardUpdateMonitor$StrongAuthTracker", lpparam.classLoader,
+					"isUnlockingWithFingerprintAllowed", new XC_MethodReplacement()
+					{
+						@Override
+						protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+							//Log.e(TAG, "isUnlockingWithFingerprintAllowed TRUE");
+							return true;
+						}
+					}
+			);
+
+			findAndHookMethod("com.android.keyguard.KeyguardUpdateMonitor", lpparam.classLoader,
+					"isUnlockingWithFingerprintAllowed", new XC_MethodReplacement()
+					{
+						@Override
+						protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+							//Log.e(TAG, "isUnlockingWithFingerprintAllowed 2 TRUE");
+							return true;
+						}
+					}
+			);
+
+			Log.e(TAG, "Hooked : " + lpparam.packageName);
+
+		}
+
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# FingerprintEnabler
+Xposed module which allow to work with FingerPrint scanner after reboot and after locking with 3-rd party applications.
+
+## Features
+
+
+## Requirments
+
+* Android 6.0
+* Xposed 78
+* Phone which support FingerPrint Scanner
